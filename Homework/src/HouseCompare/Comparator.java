bc4b5fac0920fa8c6cf4d76a72f917a836197975
Juliya <sukhorebskaya@gmail.com>
2020-12-11 15:12:12 +0300

    cannot cast to class java.lang.Comparable

---
+++ Homework/src/HouseCompare/Comparator.java
@@ -0,0 +1,5 @@
+package HouseCompare;
+
+public interface Comparator <House>{
+    int compare (House a, House b);
+}
--- Homework/src/HouseCompare/HouseCompareRun.java
+++ Homework/src/HouseCompare/HouseCompareRun.java
@@ -4,7 +4,7 @@ import java.util.TreeSet;
 
 public class HouseCompareRun {
     public static void main(String[] args) {
-        TreeSet<House> myHouseList = new TreeSet<House>();
+        TreeSet<House> myHouseList = new TreeSet<>();
         House house1 = new House(50,50000,"Minsk",false);
         House house2 = new House(60,200000,"Washington",false);
         House house3 = new House(30,28000,"Gomel",true);
@@ -13,8 +13,11 @@ public class HouseCompareRun {
         myHouseList.add(house2);
         myHouseList.add(house3);
 
+        HouseComparator hcmp = new HouseComparator();
+        TreeSet<House> houses = new TreeSet<House>(hcmp);
+
         for(House h: myHouseList)
-            System.out.println(h);
+            System.out.println(h.getArea());
     }
 
 }
